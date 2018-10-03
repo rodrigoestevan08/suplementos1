b11b94c1be4134542d3d78e7000d01982ca8901d
Bruno Pimentel <bpimentel@mastertech.tech>
2018-10-03 10:41:55 -0300

    Busca dados de produto de um CSV

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    fs.readFile('produtos.csv', {encoding: 'utf8'}, (erro, dados) => {
+        let linhas = dados.split('\n');
+        let produtos = [];
+
+        for(let linha of linhas){
+            let colunas = linha.split(';');
+            
+            produtos.push({
+                imagem: colunas[0],
+                descricao: colunas[1],
+                preco: colunas[2]
+            });
+        }
+
+        res.render('home', {'produtos': produtos});
+    });
 });
 
 app.get('/quem-eramos', (req, res) => {
