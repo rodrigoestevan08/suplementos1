03d70a73ffe7e86bcc1a6930f427fc7fc7599bff
Bruno Pimentel <bpimentel@mastertech.tech>
2018-10-04 11:18:28 -0300

    Criar painel administrativo

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const expressMongoDb = require('express-mongo-db');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 app.use(expressMongoDb('mongodb://localhost/tiojolo'));
+app.use(bodyParser.urlencoded());
 
 app.get('/', (req, res) => {
     req.db.collection('produtos').find().toArray((erro, dados) => {
         res.render('home', {'produtos': dados});
+    });
+});
+
+app.get('/admin', (req, res) => {
+    res.render('admin', {resposta: false});
+});
+
+app.post('/admin', (req, res) => {
+    req.db.collection('produtos').insert(req.body, (erro) => {
+        res.render('admin', {resposta: true});
     });
 });
 
